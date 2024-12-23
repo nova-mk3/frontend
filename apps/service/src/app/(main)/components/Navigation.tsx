0d43f85aa21e1ba4e2c2ff3f2f9c6440a0ef69c2
kwonja <kownja87@gmail.com>
2024-12-23 23:42:13 +0900

    feat: navigation ui

--- apps/service/src/app/(main)/components/Navigation.tsx
+++ apps/service/src/app/(main)/components/Navigation.tsx
@@ -5,12 +5,12 @@ import Image from "next/image";
 import {
   NavigationMenu,
   NavigationMenuContent,
-  NavigationMenuIndicator,
+  // NavigationMenuIndicator,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
-  NavigationMenuViewport,
+  // NavigationMenuViewport,
 } from "@nova/ui/components/navigation-menu";
 export default function Navigation() {
   return (
@@ -20,6 +20,7 @@ export default function Navigation() {
           <NavigationMenuTrigger className="t-m font-bold">
             소개
           </NavigationMenuTrigger>
+          {/* NavigationMenuContent 부분을 복사해 네비게이션 추가삭제가 가능함 -> 추후 리펙토링 예정 */}
           <NavigationMenuContent>
             <ul className="flex flex-col gap-[15px] p-4 md:w-[325px] lg:grid-cols-[.75fr_1fr] bg-background01">
               <NavigationMenuLink asChild>
@@ -93,7 +94,70 @@ export default function Navigation() {
             게시판
           </NavigationMenuTrigger>
           <NavigationMenuContent>
-            <NavigationMenuLink>Link</NavigationMenuLink>
+            <ul className="flex flex-col gap-[15px] p-4 md:w-[325px] lg:grid-cols-[.75fr_1fr] bg-background01">
+              <NavigationMenuLink asChild>
+                <Link href="/">
+                  <li className="flex items-center h-[60px]  rounded-lg hover:bg-ui01">
+                    <div className="flex flex-row items-center">
+                      <div className="w-[60px] h-[50px] flex justify-center items-center border-r-[1px] border-line01">
+                        <Image
+                          src="/image/Layers.svg"
+                          width={24}
+                          height={24}
+                          alt="연혁"
+                        />
+                      </div>
+                      <div className="flex flex-col ml-[16px] t-m">
+                        <p className="font-bold">통합 게시판</p>
+                        <p>질의,공지,건의 등 각종 게시판</p>
+                      </div>
+                    </div>
+                  </li>
+                </Link>
+              </NavigationMenuLink>
+
+              <NavigationMenuLink asChild>
+                <Link href="/">
+                  <li className="flex items-center w-[285px] h-[60px] hover:bg-ui01">
+                    <div className="flex flex-row items-center">
+                      <div className="w-[60px] h-[50px] flex justify-center items-center border-r-[1px] border-line01">
+                        <Image
+                          src="/image/Folder.svg"
+                          width={24}
+                          height={24}
+                          alt="연혁"
+                        />
+                      </div>
+                      <div className="flex flex-col ml-[16px] t-m">
+                        <p className="font-bold">족보 게시판</p>
+                        <p>족보 모음 게시판</p>
+                      </div>
+                    </div>
+                  </li>
+                </Link>
+              </NavigationMenuLink>
+
+              <NavigationMenuLink asChild>
+                <Link href="/">
+                  <li className="flex items-center w-[285px] h-[60px]  rounded-lg hover:bg-ui01">
+                    <div className="flex flex-row items-center">
+                      <div className="w-[60px] h-[50px] flex justify-center items-center border-r-[1px] border-line01">
+                        <Image
+                          src="/image/Picture.svg"
+                          width={24}
+                          height={24}
+                          alt="연혁"
+                        />
+                      </div>
+                      <div className="flex flex-col ml-[16px] t-m">
+                        <p className="font-bold">사진게시판</p>
+                        <p>행사 및 일상 사진</p>
+                      </div>
+                    </div>
+                  </li>
+                </Link>
+              </NavigationMenuLink>
+            </ul>
           </NavigationMenuContent>
         </NavigationMenuItem>
 
@@ -102,7 +166,49 @@ export default function Navigation() {
             가이드
           </NavigationMenuTrigger>
           <NavigationMenuContent>
-            <NavigationMenuLink>Link</NavigationMenuLink>
+            <ul className="flex flex-col gap-[15px] p-4 md:w-[325px] lg:grid-cols-[.75fr_1fr] bg-background01">
+              <NavigationMenuLink asChild>
+                <Link href="/">
+                  <li className="flex items-center h-[60px]  rounded-lg hover:bg-ui01">
+                    <div className="flex flex-row items-center">
+                      <div className="w-[60px] h-[50px] flex justify-center items-center border-r-[1px] border-line01">
+                        <Image
+                          src="/image/Check.svg"
+                          width={24}
+                          height={24}
+                          alt="연혁"
+                        />
+                      </div>
+                      <div className="flex flex-col ml-[16px] t-m">
+                        <p className="font-bold">노바 생활 가이드</p>
+                        <p>생활 규칙 가이드</p>
+                      </div>
+                    </div>
+                  </li>
+                </Link>
+              </NavigationMenuLink>
+
+              <NavigationMenuLink asChild>
+                <Link href="/">
+                  <li className="flex items-center w-[285px] h-[60px] hover:bg-ui01">
+                    <div className="flex flex-row items-center">
+                      <div className="w-[60px] h-[50px] flex justify-center items-center border-r-[1px] border-line01">
+                        <Image
+                          src="/image/People.svg"
+                          width={24}
+                          height={24}
+                          alt="연혁"
+                        />
+                      </div>
+                      <div className="flex flex-col ml-[16px] t-m">
+                        <p className="font-bold">노바 임원 지침서</p>
+                        <p>임원활동 가이드 라인</p>
+                      </div>
+                    </div>
+                  </li>
+                </Link>
+              </NavigationMenuLink>
+            </ul>
           </NavigationMenuContent>
         </NavigationMenuItem>
       </NavigationMenuList>
