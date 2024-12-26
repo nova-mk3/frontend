976c32d1dd8b5ac1d8865b4e7466c7daf710ba3a
kwonja <kownja87@gmail.com>
2024-12-26 19:25:26 +0900

    feat: header-mobile

--- apps/service/src/app/(main)/components/Header.tsx
+++ apps/service/src/app/(main)/components/Header.tsx
@@ -1,39 +1,78 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Navigation from "./Navigation";
+import Link from "next/link";
+import AppSidebar from "./Siderbar/AppSidebar";
+import { Menu } from "lucide-react";
 
 export default function Header() {
+  const [isLogin] = useState(false);
+  const [isSiderbar, setIsSiderbar] = useState(false);
+
+  const toggleSiderbar = () => {
+    setIsSiderbar((prev) => !prev);
+  };
   return (
-    <div className="flex flex-row algin border-b-[1px] border-line01 px-[17px] py-[10px]">
-      <Image
-        src="/image/Logo.svg"
-        alt="로고"
-        width={161.83}
-        height={45}
-        priority={true}
-        className="cursor-pointer"
-      />
-      <div className="flex flex-row  items-center ml-[20px] mobile:hidden">
-        <Navigation />
-      </div>
-      <div className="flex items-center ml-auto">
-        <div className="flex flex-row justify-center items-center mobile:hidden">
-          <p className="w-[60px] h-[24px] flex content-center justify-center">
-            로그인
-          </p>
-          <p className="w-[60px] h-[24px] flex content-center justify-center">
-            회원가입
-          </p>
+    <>
+      <div className="flex flex-row algin border-b-[1px] border-line01 px-[17px] py-[10px] relative">
+        <Link href="/">
+          <Image
+            src="/image/Logo.svg"
+            alt="로고"
+            width={161.83}
+            height={45}
+            priority={true}
+            className="cursor-pointer"
+          />
+        </Link>
+        <div className="flex flex-row items-center ml-[20px] mobile:hidden">
+          <Navigation />
         </div>
-        <Image
-          src="/image/hamburger.svg"
-          width={20}
-          height={20}
-          alt="햄버거"
-          className="hidden mobile:block"
-        />
+        <div className="flex items-center ml-auto">
+          {!isLogin ? (
+            <div className="flex flex-row justify-center items-center gap-[22px] mobile:hidden">
+              <p className="w-[60px] h-[24px] flex content-center justify-center cursor-pointer">
+                로그인
+              </p>
+              <p
+                className="w-[60px] h-[24px] flex content-center justify-center cursor-pointer "
+                onClick={() => alert("개발중")}
+              >
+                회원가입
+              </p>
+            </div>
+          ) : (
+            <div className="flex flex-row justify-center items-center gap-[22px] mobile:hidden">
+              <Image
+                src="/image/Search-d.svg"
+                width={48}
+                height={48}
+                alt="검색"
+              />
+              <Image
+                src="/image/Alarm-d.svg"
+                width={48}
+                height={48}
+                alt="알람"
+              />
+              <Image
+                src="/image/Profile.svg"
+                width={48}
+                height={48}
+                alt="프로필"
+                className="cursor-pointer"
+              />
+            </div>
+          )}
+          <Menu
+            size={30}
+            className="hidden mobile:block cursor-pointer"
+            onClick={() => setIsSiderbar(true)}
+          />
+        </div>
+        <AppSidebar toggle={toggleSiderbar} isOpen={isSiderbar} />
       </div>
-    </div>
+    </>
   );
 }
---
+++ apps/service/src/app/(main)/components/Siderbar/AppSidebar.tsx
@@ -0,0 +1,22 @@
+import Searchbox from "./Searchbox";
+import SidebarHeader from "./SidebarHeader";
+import React from "react";
+import SiderbarContent from "./SiderbarContent";
+
+interface SiderbarProps {
+  toggle: () => void;
+  isOpen: boolean;
+}
+export default function AppSidebar({ toggle, isOpen }: SiderbarProps) {
+  return (
+    <div
+      className={`border z-20 rounded-lg h-screen overflow-y-auto fixed right-0 top-0 bg-background01 transition-all duration-500 shadow ease-linear transform ${
+        isOpen ? "right-0 w-[100%]" : "right-[-100%] w-[100%]"
+      } max-w-[400px]`}
+    >
+      <SidebarHeader toggle={toggle} />
+      <Searchbox />
+      <SiderbarContent />
+    </div>
+  );
+}
