4b3b0ebaa1d9c919e2f23abf73e9df461c306334
kwonja <kwonja87@gmail.com>
2025-01-10 00:49:29 +0900

    fix: mobile css add

--- apps/service/src/app/(main)/archive/[id]/components/SubTitle.tsx
+++ apps/service/src/app/(main)/archive/[id]/components/SubTitle.tsx
@@ -12,7 +12,7 @@ export default function SubTitle({
   data = "2025.01.07",
 }: SubTitle) {
   return (
-    <div className="flex flex-row border-line01 border-b-[1px] py-5 gap-5">
+    <div className="flex flex-row border-line01 border-b-[1px] py-5 gap-5 mobile:flex-col">
       <p className="d-m">{title}</p>
 
       <div className="flex flex-row t-m items-end gap-3">
@@ -21,7 +21,7 @@ export default function SubTitle({
         <p>{data}</p>
       </div>
 
-      <div className="flex flex-row gap-3 items-end ml-auto">
+      <div className="flex flex-row gap-3 items-end ml-auto mobile:ml-0">
         <p>수정</p>
         <div className="w-[1px] h-[20px] bg-line01"></div>
         <p>삭제</p>
