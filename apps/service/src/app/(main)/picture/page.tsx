7a28bfede8cacdfe3b9c38e4bb36fb6d1aca1a01
kwonja <kwonja87@gmail.com>
2025-01-18 17:17:34 +0900

    feat: board category architecture set and board navigation ui

--- apps/service/src/app/(main)/picture/page.tsx
+++ apps/service/src/app/(main)/picture/page.tsx
@@ -3,7 +3,7 @@ import Title from '../archive/components/Title'
 
 export default function page() {
   return (
-    <div>
+    <div className='flex flex-col t-m w-[80%] mx-auto'>
       <Title title="사진 게시판"/>
     </div>
   )
