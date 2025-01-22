8fc53eaf5537d943ba988d7d0fb3289ea43499a2
beom710 <beom710@naver.com>
2025-01-22 18:16:22 +0900

    feat(admin): install svgr and webpack

--- apps/admin/src/app/(main)/page.tsx
+++ apps/admin/src/app/(main)/page.tsx
@@ -1,18 +1,13 @@
 // Dashboard page
 // TODO : 대시보드 창의 내용들이 확정되면 넣을것
 import React from 'react';
-import Logo from "../../../public/image/Logo.svg"
-import Image from 'next/image';
+import Logo from "@/public/image/Logo.svg"
+
 export default function Dashboard() {
   return (
     <div className="text-blue-500 mobile:bg-blue-500 font-pretendard">
       Welcome to admin! dashboard 로 만들것
-      <Image
-      src="../../../public/image/Logo.svg"
-      alt="Logo"
-      width={100}
-      height={100}
-    />
+      <Logo width={100} fill="#123123" />
     </div>
   );
 }
