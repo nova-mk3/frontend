692cd1d8588f0847b7fbf3cdbda3635214455b42
kwonja <kownja87@gmail.com>
2025-02-25 00:38:05 +0900

    fix: status error fix

--- apps/service/src/libs/utils/throwError.ts
+++ apps/service/src/libs/utils/throwError.ts
@@ -1,15 +1,14 @@
 import { ERROR_MESSAGES } from "@/src/constant/error";
 
 export const throwErrorMessage = (error: any) => {
-  console.log("하이")
-  console.log(error);
+  console.log(error.request);
   if (error.response) {
-    console.log(error.response)
     // 서버가 응답한 경우 (HTTP 상태 코드 존재)
     const status = error.response.status;
     const message = error.response.data?.message  || ERROR_MESSAGES[status] || ERROR_MESSAGES.SERVER_ERROR;
     throw new Error(message);
   } else if (error.request) {
+    console.log(error.request)
     // 네트워크 오류 (서버 응답 없음)
     throw new Error(ERROR_MESSAGES.NO_RESPONSE);
   } else {
