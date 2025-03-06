f15571812bdaf7b3090870f80e38597c3ff7574f
kwonja <kownja87@gmail.com>
2025-03-06 23:45:17 +0900

    fix: useParam suspense add

--- apps/service/src/app/(main)/board/modify/page.tsx
+++ apps/service/src/app/(main)/board/modify/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 import React from "react";
 import ModifyPage from "./ModifyPage";
+import ErrorBoundaryWrapper from "../../components/ErrorBoundaryWrapper";
 export const dynamic = "force-dynamic";
 export default function page() {
-  return <ModifyPage />;
+  return (
+    <ErrorBoundaryWrapper>
+      <ModifyPage />
+    </ErrorBoundaryWrapper>
+  );
 }
