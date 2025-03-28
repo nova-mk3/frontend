0a4b75e051e64e8ba32d8c95d29879c1fc31a6d6
kwonja <kownja87@gmail.com>
2025-03-28 12:02:51 +0900

    fix:  .vscode from version control

--- apps/service/src/schema/signin.schema.ts
+++ apps/service/src/schema/signin.schema.ts
@@ -10,9 +10,9 @@ export const SigninSchema = z.object({
     .regex(/^\d+$/, { message: "학번은 숫자만 입력할 수 있습니다." }),
   password: z
     .string()
-    .min(8, { message: "비밀번호는 8글자 이상이어야 합니다." })
+    .min(6, { message: "비밀번호는 6글자 이상이어야 합니다." })
     .refine((password) => isStrongPassword(password), {
-      message: "소문자, 대문자, 숫자, 특수문자를 모두 포함해야 합니다.",
+      message: "소문자, 숫자, 특수문자를 모두 포함해야 합니다.",
     }),
 });
 
