a59badac0823f0d927889f594a7b62030d0de081
beom710 <beom710@naver.com>
2025-03-29 20:09:41 +0900

    feat: manageMember Apis

--- apps/admin/src/api/main/member/manageMembersApi.ts
+++ apps/admin/src/api/main/member/manageMembersApi.ts
@@ -1,9 +1,64 @@
 import { Authapi } from "@/src/api/core";
 import { throwError } from "@/src/utils/throwError";
+import { PutMemberInfoRequest } from "@/src/types/manageMember";
 
 export async function GetAllMembers() {
     try{
-        const response = await Authapi.get(`/nova/executive-histories/members`)
+        const response = await Authapi.get(`/nova/admin/members`)
+        return response.data.data
+    }catch(error:unknown){
+        throwError(error)
+    }
+}
+
+export async function PutMemberInfo(memberId: number, request: PutMemberInfoRequest) {
+    try{
+        const response = await Authapi.put(`/nova/members/members/${memberId}`, request)
+        return response.data.data
+    }catch(error:unknown){
+        throwError(error)
+    }
+}
+
+export async function PutMemberGraduation(memberId: number, Graduation: boolean) {
+    try{
+        const response = await Authapi.put(`/nova/admin/members/${memberId}/graduation?isGraduation=${Graduation}`)
+        return response.data.data
+    }catch(error:unknown){
+        throwError(error)
+    }
+}
+
+export async function PutMemberGrade(memberId: number, grade: number) {
+    try{
+        const response = await Authapi.put(`/nova/admin/members/${memberId}/grade?grade=${grade}`)
+        return response.data.data
+    }catch(error:unknown){
+        throwError(error)
+    }
+}
+
+export async function PutMemberAbsence(memberId: number , absence: boolean) {
+    try{
+        const response = await Authapi.put( `/nova/admin/members/${memberId}/absence?isAbsent=${absence}`)
+        return response.data.data
+    }catch(error:unknown){
+        throwError(error)
+    }
+}
+
+export async function DeleteMember(memberId: number) {
+    try{
+        const response = await Authapi.put(`/nova/admin/members/${memberId}/deleted`)
+        return response.data.data
+    }catch(error:unknown){
+        throwError(error)
+    }
+}
+
+export async function PutAllMemberSemester(){
+    try{
+        const response = await Authapi.put(`/nova/admin/members/semester`)
         return response.data.data
     }catch(error:unknown){
         throwError(error)
--- apps/admin/src/types/manageMember.ts
+++ apps/admin/src/types/manageMember.ts
@@ -25,3 +25,30 @@ export interface ManageMemberCardModalProps {
     open : boolean;
     onClose : () => void;
 }
+
+export interface PutMemberInfoRequest {
+    updateMemberProfileRequest : updateMemberProfileRequest;
+    updateMemberStatusRequest : updateGraduationRequest;
+}
+
+export interface updateMemberProfileRequest {
+    name : string
+    studentNumber: string,
+    graduation: boolean,
+    grade: number,
+    semester: number,
+    absence: boolean,
+    birth: string,
+    profilePhoto: string,
+    phone: string,
+    introduction: string
+}
+
+export interface updateGraduationRequest {
+    year: number,
+    work: boolean,
+    job: string,
+    contact : boolean,
+    contactInfo: string,
+    contactDescription: string
+  }
