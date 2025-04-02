7fa4b626995059213d817cb645a83dd4346aa768
kwonja <kownja87@gmail.com>
2025-04-02 21:55:55 +0900

    feat: 안쓰는 폰트 삭제

--- apps/service/src/theme/font.ts
+++ apps/service/src/theme/font.ts
@@ -2,21 +2,21 @@ import localFont from "next/font/local";
 
 export const pretendard = localFont({
   src: [
-    {
-      path: "../../public/fonts/Pretendard-Thin.subset.woff2",
-      weight: "100",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/Pretendard-ExtraLight.subset.woff2",
-      weight: "200",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/Pretendard-Light.subset.woff2",
-      weight: "300",
-      style: "normal",
-    },
+    // {
+    //   path: "../../public/fonts/Pretendard-Thin.subset.woff2",
+    //   weight: "100",
+    //   style: "normal",
+    // },
+    // {
+    //   path: "../../public/fonts/Pretendard-ExtraLight.subset.woff2",
+    //   weight: "200",
+    //   style: "normal",
+    // },
+    // {
+    //   path: "../../public/fonts/Pretendard-Light.subset.woff2",
+    //   weight: "300",
+    //   style: "normal",
+    // },
     {
       path: "../../public/fonts/Pretendard-Regular.subset.woff2",
       weight: "400",
@@ -37,38 +37,38 @@ export const pretendard = localFont({
       weight: "700",
       style: "normal",
     },
-    {
-      path: "../../public/fonts/Pretendard-ExtraBold.subset.woff2",
-      weight: "800",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/Pretendard-Black.subset.woff2",
-      weight: "900",
-      style: "normal",
-    },
+    // {
+    //   path: "../../public/fonts/Pretendard-ExtraBold.subset.woff2",
+    //   weight: "800",
+    //   style: "normal",
+    // },
+    // {
+    //   path: "../../public/fonts/Pretendard-Black.subset.woff2",
+    //   weight: "900",
+    //   style: "normal",
+    // },
   ],
   variable: "--font-pretendard",
-  display: "fallback",
+  display: "swap",
 });
 
 export const paperlogy = localFont({
   src: [
-    {
-      path: "../../public/fonts/Paperlogy-1Thin.woff2",
-      weight: "100",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/Paperlogy-2ExtraLight.woff2",
-      weight: "200",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/Paperlogy-3Light.woff2",
-      weight: "300",
-      style: "normal",
-    },
+    // {
+    //   path: "../../public/fonts/Paperlogy-1Thin.woff2",
+    //   weight: "100",
+    //   style: "normal",
+    // },
+    // {
+    //   path: "../../public/fonts/Paperlogy-2ExtraLight.woff2",
+    //   weight: "200",
+    //   style: "normal",
+    // },
+    // {
+    //   path: "../../public/fonts/Paperlogy-3Light.woff2",
+    //   weight: "300",
+    //   style: "normal",
+    // },
     {
       path: "../../public/fonts/Paperlogy-4Regular.woff2",
       weight: "400",
@@ -79,11 +79,11 @@ export const paperlogy = localFont({
       weight: "500",
       style: "normal",
     },
-    {
-      path: "../../public/fonts/Paperlogy-6SemiBold.woff2",
-      weight: "600",
-      style: "normal",
-    },
+    // {
+    //   path: "../../public/fonts/Paperlogy-6SemiBold.woff2",
+    //   weight: "600",
+    //   style: "normal",
+    // },
     {
       path: "../../public/fonts/Paperlogy-7Bold.woff2",
       weight: "700",
@@ -94,12 +94,12 @@ export const paperlogy = localFont({
       weight: "800",
       style: "normal",
     },
-    {
-      path: "../../public/fonts/Paperlogy-9Black.woff2",
-      weight: "900",
-      style: "normal",
-    },
+    // {
+    //   path: "../../public/fonts/Paperlogy-9Black.woff2",
+    //   weight: "900",
+    //   style: "normal",
+    // },
   ],
-  display: "fallback",
+  display: "swap",
   variable: "--font-paperlogy",
 });
