5d2c64bf1bfb743930f14ad36648fd83007fc6e8
beom710 <beom710@naver.com>
2025-04-07 01:43:00 +0900

    feat: 멤버 상태변경 쿼리 추가가

--- apps/admin/src/query/manageMembersQueries.ts
+++ apps/admin/src/query/manageMembersQueries.ts
@@ -1,5 +1,5 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
-import { GetAllMembers, GetMemberInfo, PutAllMemberSemester, PutMemberInfo } from '@/src/api/main/member/manageMembersApi';
+import { GetAllMembers, GetMemberInfo, PutAllMemberSemester, PutMemberAbsence, PutMemberGrade, PutMemberGraduation, PutMemberInfo } from '@/src/api/main/member/manageMembersApi';
 import { ManageMember, ManageMemberInfoResponse, PutMemberInfoRequest } from '@/src/types/manageMember';
 
 export const manageMembersKeys = {
@@ -51,6 +51,47 @@ export const usePutAllMemberSemesterMutation = () => {
         },
         onError: (error) => {
             console.error("회원 정보 수정 실패:", error);
+        },
+    });
+}
+
+export const usePutMemberGraduationMutation = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: async (props: { memberId: string; Graduation: boolean }) => {
+            const { memberId, Graduation } = props;
+            return PutMemberGraduation(memberId, Graduation);
+        },
+        onSuccess: (_, variables) => {
+            queryClient.invalidateQueries({ queryKey: manageMembersKeys.detail(variables.memberId) });
+            queryClient.invalidateQueries({ queryKey: manageMembersKeys.list() });
+        },
+    });
+}
+
+export const usePutMemberGradeMutation = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: async (props: { memberId: string; grade: number }) => {
+            const { memberId, grade } = props;
+            return PutMemberGrade(memberId, grade);
+        },
+        onSuccess: (_, variables) => {
+            queryClient.invalidateQueries({ queryKey: manageMembersKeys.detail(variables.memberId) });
+            queryClient.invalidateQueries({ queryKey: manageMembersKeys.list() });
+        },
+    });
+}
+export const usePutMemberAbsenceMutation = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: async (props: { memberId: string; absence: boolean }) => {
+            const { memberId, absence } = props;
+            return PutMemberAbsence(memberId, absence);
+        },
+        onSuccess: (_, variables) => {
+            queryClient.invalidateQueries({ queryKey: manageMembersKeys.detail(variables.memberId) });
+            queryClient.invalidateQueries({ queryKey: manageMembersKeys.list() });
         },
     });
 }
