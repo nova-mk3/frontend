32618897c35fcfe9767d90965c88e7404aee7081
beom710 <beom710@naver.com>
2025-04-07 22:38:08 +0900

    feat: 임시 페이지 변경경

--- apps/admin/src/app/admin/(main)/serverLog/page.tsx
+++ apps/admin/src/app/admin/(main)/serverLog/page.tsx
@@ -2,10 +2,12 @@
 // TODO : 서버에서의 Log들을 화면에 띄워주고 DB에 명령어를 통해 접근이 가능할것
 
 export default function ServerLog() {
-    return (
-      <div className="text-blue-500 mobile:bg-blue-500 font-pretendard">
-        ServerLog Page
-      </div>
-    );
-  }
-  
+  return (
+    <div
+      className="font-pretendard font-bold text-2xl flex items-center justify-center"
+      style={{ width: "1400px", height: "800px" }}
+    >
+      ServerLog 페이지 준비중입니다.
+    </div>
+  );
+}
