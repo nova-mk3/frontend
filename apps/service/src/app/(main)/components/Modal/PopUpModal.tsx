b71d572ee0021bb2870ec9d4b31f089dfd73bcde
kwonja <kownja87@gmail.com>
2025-05-02 20:33:28 +0900

    feat: 업데이트 팝업

--- apps/service/src/app/(main)/components/Modal/PopUpModal.tsx
+++ apps/service/src/app/(main)/components/Modal/PopUpModal.tsx
@@ -5,17 +5,15 @@ import React, { useEffect, useState } from "react";
 
 interface ModalProps {
   isOpen: boolean;
-  title: string;
-  subtitle: string;
   onClose?: () => void;
   onAction?: () => void;
+  children: React.ReactNode;
 }
-export default function Modal({
+export default function PopUpModal({
   isOpen,
-  title,
-  subtitle,
   onClose,
   onAction,
+  children,
 }: ModalProps) {
   const [shouldRender, setShouldRender] = useState(false);
   useEffect(() => {
@@ -34,37 +32,33 @@ export default function Modal({
   return (
     <div
       data-state={isOpen ? "open" : "closed"}
-      className={cn(
-        "fixed inset-0 z-50 bg-black/80  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0"
-      )}
+      className={cn("fixed inset-0 z-50   bg-black/5")}
     >
       <div
         data-state={isOpen ? "open" : "closed"}
         className={cn(
           "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background01 p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg",
-          "w-[400px] h-[204px] p-8 flex flex-col"
+          "min-w-[400px] min-h-[204px] p-8 flex flex-col"
         )}
       >
-        <p className="t-l ">{title}</p>
-        <p className="t-m text-text02">{subtitle}</p>
+        {children}
+
         <div className="flex flex-row gap-2 ml-auto mt-auto">
           <Button
             variant="text"
-            className="w-[72px]"
             onClick={() => {
               if (onClose) onClose();
             }}
           >
-            취소
+            닫기
           </Button>
           <Button
             variant="default"
-            className="w-[72px]"
             onClick={() => {
               if (onAction) onAction();
             }}
           >
-            확인
+            다시 보지 않기
           </Button>
         </div>
       </div>
