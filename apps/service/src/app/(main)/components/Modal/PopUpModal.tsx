999eb15095329a6541d0db4f88a22051360bd262
kwonja <kownja87@gmail.com>
2025-05-02 21:07:42 +0900

    feat: 팝업창 메인페이지 상단 고정

--- apps/service/src/app/(main)/components/Modal/PopUpModal.tsx
+++ apps/service/src/app/(main)/components/Modal/PopUpModal.tsx
@@ -32,35 +32,30 @@ export default function PopUpModal({
   return (
     <div
       data-state={isOpen ? "open" : "closed"}
-      className={cn("fixed inset-0 z-50   bg-black/5")}
+      className={cn(
+        "absolute left-[50%] top-[10%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background01 p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg",
+        "min-w-[400px] min-h-[204px] p-8 flex flex-col"
+      )}
     >
-      <div
-        data-state={isOpen ? "open" : "closed"}
-        className={cn(
-          "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background01 p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg",
-          "min-w-[400px] min-h-[204px] p-8 flex flex-col"
-        )}
-      >
-        {children}
+      {children}
 
-        <div className="flex flex-row gap-2 ml-auto mt-auto">
-          <Button
-            variant="text"
-            onClick={() => {
-              if (onClose) onClose();
-            }}
-          >
-            닫기
-          </Button>
-          <Button
-            variant="default"
-            onClick={() => {
-              if (onAction) onAction();
-            }}
-          >
-            다시 보지 않기
-          </Button>
-        </div>
+      <div className="flex flex-row gap-2 ml-auto mt-auto">
+        <Button
+          variant="text"
+          onClick={() => {
+            if (onClose) onClose();
+          }}
+        >
+          닫기
+        </Button>
+        <Button
+          variant="default"
+          onClick={() => {
+            if (onAction) onAction();
+          }}
+        >
+          다시 보지 않기
+        </Button>
       </div>
     </div>
   );
--- apps/service/src/app/(main)/users/message/page.tsx
+++ apps/service/src/app/(main)/users/message/page.tsx
@@ -3,70 +3,70 @@ import React from "react";
 import AlarmList from "./AlarmList";
 import { AlarmListItemProps } from "./AlarmListItem";
 
-export const dummyAlarms: AlarmListItemProps[] = [
-  {
-    id: "1",
-    isRead: false,
-    message: "홍길동님이 회원님의 게시글에 댓글을 남겼습니다.",
-    createAt: "1일 전",
-  },
-  {
-    id: "2",
-    isRead: true,
-    message: "새로운 팔로워가 생겼습니다.",
-    createAt: "2일 전",
-  },
-  {
-    id: "3",
-    isRead: false,
-    message: "공지사항이 등록되었습니다. 확인해보세요.",
-    createAt: "3일 전",
-  },
-  {
-    id: "4",
-    isRead: true,
-    message: "비밀번호 변경이 완료되었습니다.",
-    createAt: "5일 전",
-  },
-  {
-    id: "5",
-    isRead: false,
-    message: "이벤트에 당첨되셨습니다! 자세히 확인하세요.",
-    createAt: "1주 전",
-  },
-  {
-    id: "6",
-    isRead: true,
-    message: "프로필 사진이 변경되었습니다.",
-    createAt: "2주 전",
-  },
-  {
-    id: "7",
-    isRead: false,
-    message: "새로운 댓글이 달렸습니다.",
-    createAt: "3주 전",
-  },
-  {
-    id: "8",
-    isRead: true,
-    message: "1개월 동안 활동이 없었습니다. 다시 돌아오세요!",
-    createAt: "1개월 전",
-  },
-  {
-    id: "9",
-    isRead: false,
-    message: "보안 알림: 로그인 시도가 감지되었습니다.",
-    createAt: "6개월 전",
-  },
-  {
-    id: "10",
-    isRead: true,
-    message: "회원 가입 1주년을 축하드립니다!",
-    createAt: "1년 전",
-  },
-];
-
 export default function page() {
+  const dummyAlarms: AlarmListItemProps[] = [
+    {
+      id: "1",
+      isRead: false,
+      message: "홍길동님이 회원님의 게시글에 댓글을 남겼습니다.",
+      createAt: "1일 전",
+    },
+    {
+      id: "2",
+      isRead: true,
+      message: "새로운 팔로워가 생겼습니다.",
+      createAt: "2일 전",
+    },
+    {
+      id: "3",
+      isRead: false,
+      message: "공지사항이 등록되었습니다. 확인해보세요.",
+      createAt: "3일 전",
+    },
+    {
+      id: "4",
+      isRead: true,
+      message: "비밀번호 변경이 완료되었습니다.",
+      createAt: "5일 전",
+    },
+    {
+      id: "5",
+      isRead: false,
+      message: "이벤트에 당첨되셨습니다! 자세히 확인하세요.",
+      createAt: "1주 전",
+    },
+    {
+      id: "6",
+      isRead: true,
+      message: "프로필 사진이 변경되었습니다.",
+      createAt: "2주 전",
+    },
+    {
+      id: "7",
+      isRead: false,
+      message: "새로운 댓글이 달렸습니다.",
+      createAt: "3주 전",
+    },
+    {
+      id: "8",
+      isRead: true,
+      message: "1개월 동안 활동이 없었습니다. 다시 돌아오세요!",
+      createAt: "1개월 전",
+    },
+    {
+      id: "9",
+      isRead: false,
+      message: "보안 알림: 로그인 시도가 감지되었습니다.",
+      createAt: "6개월 전",
+    },
+    {
+      id: "10",
+      isRead: true,
+      message: "회원 가입 1주년을 축하드립니다!",
+      createAt: "1년 전",
+    },
+  ];
+
   return (
     <div>
       <div className="bg-text02 px-3 py-2 max-w-screen-xl mobile:py-5">
