157a17cc9b6926a652bf7c311740dac5243a93c3
kwonja <kownja87@gmail.com>
2025-06-08 22:01:59 +0900

    refactor: 폰트 깜빡임 방지

--- apps/service/src/shared/ui/framePictureSection/FramePictureSection.tsx
+++ apps/service/src/shared/ui/framePictureSection/FramePictureSection.tsx
@@ -13,7 +13,7 @@ export default function FramePictureSection({
     <div className={cn(className)} {...props}>
       {/* TODO : 나중에 시간되면 등장 애니메이션 같은게 있으면 좋겠음. */}
       {/* TODO : 나중에 애니메이션을 넣어야함 3d카드 느낌으로 */}
-      <div className="d-l !font-bold mt-4 mb-4 text-center">
+      <div className="d-l mt-4 mb-4 text-center font-pretendard">
         CBNU software club NOVA
       </div>
       <div className="relative w-screen flex justify-center">
