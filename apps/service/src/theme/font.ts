0d209c2cfb1158bc4a2016bf2596e798126530fd
kwonja <kownja87@gmail.com>
2025-06-08 15:58:11 +0900

    refactor: woff2 폰트 용량 최적화

--- apps/service/src/theme/font.ts
+++ apps/service/src/theme/font.ts
@@ -3,103 +3,105 @@ import localFont from "next/font/local";
 export const pretendard = localFont({
   src: [
     // {
-    //   path: "../../public/fonts/Pretendard-Thin.subset.woff2",
+    //   path: "./fonts/Pretendard-Thin.subset.woff2",
     //   weight: "100",
     //   style: "normal",
     // },
     // {
-    //   path: "../../public/fonts/Pretendard-ExtraLight.subset.woff2",
+    //   path: "./fonts/Pretendard-ExtraLight.subset.woff2",
     //   weight: "200",
     //   style: "normal",
     // },
     // {
-    //   path: "../../public/fonts/Pretendard-Light.subset.woff2",
+    //   path: "./fonts/Pretendard-Light.subset.woff2",
     //   weight: "300",
     //   style: "normal",
     // },
     {
-      path: "../../public/fonts/Pretendard-Regular.subset.woff2",
+      path: "./fonts/Pretendard-Regular.subset.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../../public/fonts/Pretendard-Medium.subset.woff2",
+      path: "./fonts/Pretendard-Medium.subset.woff2",
       weight: "500",
       style: "normal",
     },
     {
-      path: "../../public/fonts/Pretendard-SemiBold.subset.woff2",
+      path: "./fonts/Pretendard-SemiBold.subset.woff2",
       weight: "600",
       style: "normal",
     },
     {
-      path: "../../public/fonts/Pretendard-Bold.subset.woff2",
+      path: "./fonts/Pretendard-Bold.subset.woff2",
       weight: "700",
       style: "normal",
     },
     // {
-    //   path: "../../public/fonts/Pretendard-ExtraBold.subset.woff2",
+    //   path: "./fonts/Pretendard-ExtraBold.subset.woff2",
     //   weight: "800",
     //   style: "normal",
     // },
     // {
-    //   path: "../../public/fonts/Pretendard-Black.subset.woff2",
+    //   path: "./fonts/Pretendard-Black.subset.woff2",
     //   weight: "900",
     //   style: "normal",
     // },
   ],
   variable: "--font-pretendard",
   display: "swap",
+  preload: true,
 });
 
 export const paperlogy = localFont({
   src: [
     // {
-    //   path: "../../public/fonts/Paperlogy-1Thin.woff2",
+    //   path: "./fonts/Paperlogy-1Thin.woff2",
     //   weight: "100",
     //   style: "normal",
     // },
     // {
-    //   path: "../../public/fonts/Paperlogy-2ExtraLight.woff2",
+    //   path: "./fonts/Paperlogy-2ExtraLight.woff2",
     //   weight: "200",
     //   style: "normal",
     // },
     // {
-    //   path: "../../public/fonts/Paperlogy-3Light.woff2",
+    //   path: "./fonts/Paperlogy-3Light.woff2",
     //   weight: "300",
     //   style: "normal",
     // },
     {
-      path: "../../public/fonts/Paperlogy-4Regular.woff2",
+      path: "./fonts/Paperlogy-4Regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../../public/fonts/Paperlogy-5Medium.woff2",
+      path: "./fonts/Paperlogy-5Medium.woff2",
       weight: "500",
       style: "normal",
     },
     // {
-    //   path: "../../public/fonts/Paperlogy-6SemiBold.woff2",
+    //   path: "./fonts/Paperlogy-6SemiBold.woff2",
     //   weight: "600",
     //   style: "normal",
     // },
     {
-      path: "../../public/fonts/Paperlogy-7Bold.woff2",
+      path: "./fonts/Paperlogy-7Bold.woff2",
       weight: "700",
       style: "normal",
     },
     {
-      path: "../../public/fonts/Paperlogy-8ExtraBold.woff2",
+      path: "./fonts/Paperlogy-8ExtraBold.woff2",
       weight: "800",
       style: "normal",
     },
     // {
-    //   path: "../../public/fonts/Paperlogy-9Black.woff2",
+    //   path: "./fonts/Paperlogy-9Black.woff2",
     //   weight: "900",
     //   style: "normal",
     // },
   ],
   display: "swap",
   variable: "--font-paperlogy",
+  preload: true,
 });
