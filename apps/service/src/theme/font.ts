2babb652860503d99e3f14ba7dc1ebe3fd544b0f
kwonja <kownja87@gmail.com>
2025-06-10 15:28:46 +0900

    refactor: lcp 테스트

--- apps/service/src/theme/font.ts
+++ apps/service/src/theme/font.ts
@@ -3,33 +3,33 @@ import localFont from "next/font/local";
 /**
  * localFont를 사용하면 빌드시 캐싱을 진행한다!
  */
-// export const pretendard = localFont({
-//   src: [
-//     {
-//       path: "./fonts/Pretendard-Regular.subset.woff2",
-//       weight: "400",
-//       style: "normal",
-//     },
-//     // {
-//     //   path: "./fonts/Pretendard-Medium.subset.woff2",
-//     //   weight: "500",
-//     //   style: "normal",
-//     // },
-//     // {
-//     //   path: "./fonts/Pretendard-SemiBold.subset.woff2",
-//     //   weight: "600",
-//     //   style: "normal",
-//     // },
-//     {
-//       path: "./fonts/Pretendard-Bold.subset.woff2",
-//       weight: "700",
-//       style: "normal",
-//     },
-//   ],
-//   variable: "--font-pretendard",
-//   display: "swap",
-//   preload: true,
-// });
+export const pretendard = localFont({
+  src: [
+    {
+      path: "./fonts/Pretendard-Regular.subset.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    // {
+    //   path: "./fonts/Pretendard-Medium.subset.woff2",
+    //   weight: "500",
+    //   style: "normal",
+    // },
+    // {
+    //   path: "./fonts/Pretendard-SemiBold.subset.woff2",
+    //   weight: "600",
+    //   style: "normal",
+    // },
+    {
+      path: "./fonts/Pretendard-Bold.subset.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-pretendard",
+  display: "swap",
+  preload: true,
+});
 
 export const paperlogy = localFont({
   src: [
@@ -38,22 +38,22 @@ export const paperlogy = localFont({
       weight: "400",
       style: "normal",
     },
-    // {
-    //   path: "./fonts/Paperlogy-5Medium.woff2",
-    //   weight: "500",
-    //   style: "normal",
-    // },
+    {
+      path: "./fonts/Paperlogy-5Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
 
     {
       path: "./fonts/Paperlogy-7Bold.woff2",
       weight: "700",
       style: "normal",
     },
-    // {
-    //   path: "./fonts/Paperlogy-8ExtraBold.woff2",
-    //   weight: "800",
-    //   style: "normal",
-    // },
+    {
+      path: "./fonts/Paperlogy-8ExtraBold.woff2",
+      weight: "800",
+      style: "normal",
+    },
   ],
   display: "swap",
   variable: "--font-paperlogy",
