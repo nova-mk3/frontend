1b5562c7719638c0c054935fad693a4142ace2af
kwonja <kownja87@gmail.com>
2025-06-15 15:24:25 +0900

    feat: next.config.js imagesize 수정

--- apps/service/next.config.ts
+++ apps/service/next.config.ts
@@ -33,7 +33,7 @@ const nextConfig: NextConfig = {
       },
     ],
     deviceSizes: [320, 420, 700, 1024, 1200],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256],
   },
   compiler: isProd
     ? {
